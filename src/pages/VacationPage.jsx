10c776bb3a33f768b9c97291bb15f43db9416ce3
Naylune <ajlunalmazova324@gmail.com>
2025-09-05 16:38:28 +0600

    first

--- src/pages/VacationPage.jsx
+++ src/pages/VacationPage.jsx
@@ -3,6 +3,19 @@ import styles from "../styles/Vacation.module.scss"
 
 export const VacationPage = () => {
     return (
-        <div>Vacation</div>
+        <div>
+            <header>
+                <img src="./assets/logo (1).svg" alt="" />
+
+                <input placeholder="Найти товар" type="text" />
+
+                <div>
+                    <img src="./assets/MainMenu (1).svg" alt="" />
+                    <img src="./assets/MainMenu (2).svg" alt="" />
+                </div>
+            </header>
+
+            <div></div>
+        </div>
     )
 }
